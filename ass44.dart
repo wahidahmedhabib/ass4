a42e315e1075960d26760140f6313121ab0a159e
wahidahmedhabib <wahidahmed221098@gmail.com>
2021-10-08 21:53:26 +0100

    assigment 4

---
+++ ass44.dart
@@ -0,0 +1,36 @@
+
+// Q NO 4 AND 01
+void main(){
+ print ('hmza ${abc(33,43,45)}');
+  print('waqs ${abc(33,44,45)}');
+  print('javed ${abc(41,44,47)}');
+  
+}
+dynamic abc(var phy,var bio,var math){
+  var total=phy+bio+math;
+  return total;
+}
+// //Q NO# 08  AND 04
+// void main(){ 
+//   var bike1 =Bike();
+//   bike1.name ='honda';
+//   bike1.disply();
+//    print(how (78,98));
+//   print(how(67,76));
+//   print(how(32,45));
+// }
+ 
+// class Bike{
+//   var name='';
+//   var modle='';
+//   dynamic disply(){
+//     print('this is ${name} bike');
+//   }
+// }
+// dynamic how(var math,var phy ){
+//   var total= math+phy;
+// //   var math=72;
+// //   var phy=83;
+//   //var sum=math+phy;
+//   return total;
+// }
